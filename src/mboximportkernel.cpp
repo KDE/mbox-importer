41cbfcc6a4f47ea07031a015b5e79e84f0edc383
Laurent Montel <montel@kde.org>
2020-11-25 20:53:41 +0100

    Pedantic

--- src/mboximportkernel.cpp
+++ src/mboximportkernel.cpp
@@ -104,11 +104,11 @@ Akonadi::Collection::Id MBoxImporterKernel::lastSelectedFolder()
 
 void MBoxImporterKernel::setLastSelectedFolder(Akonadi::Collection::Id col)
 {
-    Q_UNUSED(col);
+    Q_UNUSED(col)
 }
 
 void MBoxImporterKernel::expunge(Akonadi::Collection::Id col, bool sync)
 {
-    Q_UNUSED(col);
-    Q_UNUSED(sync);
+    Q_UNUSED(col)
+    Q_UNUSED(sync)
 }
