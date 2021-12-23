bd119947e4fa1026473eaca870bd64d49596f743
Laurent Montel <montel@kde.org>
2021-12-23 08:00:17 +0100

    GIT_SILENT: it's enabled by default too in qt6

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,8 @@ int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
-#endif
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+#endif
     QApplication app(argc, argv);
     app.setDesktopFileName(QStringLiteral("org.kde.mboximporter"));
     KCrash::initialize();
