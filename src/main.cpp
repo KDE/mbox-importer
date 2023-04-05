e05685668b93406e3ecd4df307ad2809de7f4185
Laurent Montel <montel@kde.org>
2023-04-05 08:44:51 +0200

    Remove qt < 6.0 code

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,6 @@
 
 int main(int argc, char *argv[])
 {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
-    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
-#endif
     QApplication app(argc, argv);
     app.setDesktopFileName(QStringLiteral("org.kde.mboximporter"));
     KCrash::initialize();
